d6966ed96bbd677999831f94639e83fb979f3a31
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-06-28 02:59:55 +0100

    set-up

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
+import { PrismaModule } from "src/prisma/prisma.module";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Module({
-    providers: [AuthService],
+    providers: [AuthService, PrismaService],
     controllers: [AuthController],
     imports: []
 })
